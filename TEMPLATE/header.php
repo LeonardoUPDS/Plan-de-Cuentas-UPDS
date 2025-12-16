c605b60353f0bf5ed9e77f7e0aaab7782ba2f233
LeonardoUPDS <sc.leonardo.vargas.m@upds.net.bo>
2025-12-16 09:05:41 -0400

    cambio header direncionamiento hecho

--- TEMPLATE/header.php
+++ TEMPLATE/header.php
@@ -30,10 +30,10 @@ $appRoot = '/' . ($parts[0] ?? '');
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>/SECCION/USUARIO/index.php?>">Usuarios</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>/SECCION/CURSOS/index.php?>">Cursos</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>/SECCION/ROL/index.php">Rol</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>/SECCION/PERMISO/index.php">Permiso</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL?>/SECCION/USUARIO/index.php">Usuarios</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL?>/SECCION/CURSOS/index.php">Cursos</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL?>/SECCION/ROL/index.php">Rol</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= BASE_URL?>/SECCION/PERMISO/index.php">Permiso</a></li>
           </ul>
         </div>
       </div>
