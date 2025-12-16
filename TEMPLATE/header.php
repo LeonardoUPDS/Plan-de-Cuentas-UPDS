4e9dea967cd8791a8bd6b3cef4040852b4816d06
LeonardoUPDS <sc.leonardo.vargas.m@upds.net.bo>
2025-12-16 09:23:58 -0400

    Cambios del sitio web

--- TEMPLATE/header.php
+++ TEMPLATE/header.php
@@ -24,7 +24,7 @@ $appRoot = '/' . ($parts[0] ?? '');
   <body>
     <nav class="navbar navbar-expand-lg navbar-light mb-3 border-bottom">
       <div class="container-fluid">
-        <a class="navbar-brand" href="<?= htmlspecialchars($appRoot . '/index.php') ?>">Plataforma Educativa</a>
+        <a class="navbar-brand" href="<?= BASE_URL?>/index.php">Plataforma Educativa</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
